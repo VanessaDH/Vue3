1d84b66d12324c25e72f87aef6a586f7684f0a5d
VanessaDH <120335719+VanessaDH@users.noreply.github.com>
2026-05-28 13:01:54 +0200

    Todo Task Manager Project Progress

---
+++ todo-task-manager/src/components/TodoFilter.vue
@@ -0,0 +1,18 @@
+<script setup>
+  import {ref} from 'vue'
+  const filterText = ref('')
+
+  const emit =defineEmits(['filter-change'])
+
+</script>
+
+<template>
+  <div class="todo-filter">
+    <label>Filter todos:</label>
+    <input placeholder="Search…" /> <!--v-Model to be used-->
+    <span class="filter-hint">Showing: ""</span>
+  </div>
+</template>
+
+<style scoped>
+</style>
